1a6db6d7552c23ebe6da5bbabb0368e8df97829b
Melad Abulail <melad.k.abulail@gmail.com>
2024-10-30 14:56:46 +0300

    Update to React 18 rendering method

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import React from 'react';
 
-
-function App() {
-
-
+const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
   );
-}
+};
 
 export default App;
